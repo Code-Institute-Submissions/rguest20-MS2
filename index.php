bbd458b89a305b9c19fd343e296729b1a21853d1
rguest20 <67912282+rguest20@users.noreply.github.com>
2020-08-13 22:58:07 +0100

    Started to create content. Styling started. Orientation specific.

--- index.php
+++ index.php
@@ -10,15 +10,37 @@ if (!isset($_SESSION['UserData']['Username'])) {
 
 <head>
   <meta charset="utf-8">
-  <title></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Negotiation</title>
+  <link href="./css/style2.css" rel="stylesheet">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 
 <body>
-  <p>Congratulation! You have logged into password protected page. <a href="logout.php">Click here</a> to Logout.</p>
+  <div id="portrait">
+    <h1>Please Rotate Your Device To Play</h1>
+    <p><a href="logout.php">Click here</a> to Logout.</p>
+  </div>
+  <div id="landscape">
+    <div id="negotiationwrapper border" class="row">
+      <div id="negotiationwindow" class="col-12 row border">
+        <div id="negotiationhostagebox" class="col-3 border">
+          <p>something</p>
+        </div>
+        <div id="interface" class="col-6 border">
+          <p>something</p>
+        </div>
+        <div id="diceroller" class="col-3 border">
+          <p>something</p>
+        </div>
+      </div>
+    </div>
+    <p><a href="logout.php">Click here</a> to Logout.</p>
+  </div>
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
--- login.php
+++ login.php
@@ -25,46 +25,38 @@
 <html>
 
 <head>
-	<meta charset="utf-8">
-	<title>Disclaimer/Login</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-	<link href="./css/style.css" rel="stylesheet">
+  <meta charset="utf-8">
+  <title>Disclaimer/Login</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <link href="./css/style.css" rel="stylesheet">
 </head>
 
 <body>
-	<div id="Frame0">
-		<h1>Milestone Project 2</h1>
-		This page is accessible by permission only. To obtain permission please email <a href="mailto:info@r-guest-developer.world">here</a>
-
-	</div>
-	<br>
-	<form action="" method="post" name="Login_Form">
-		<?php if (isset($msg)) {
+  <div id="content" class="row">
+    <div id="jumbotron" class="jumbotron col-12 offset-md-2 col-md-8">
+      <h1 id="title">Milestone Project 2</h1>
+      <p id="titlesub">This page is accessible by permission only. To obtain permission please email <a href="mailto:info@r-guest-developer.world">here</a></p>
+      <br>
+      <form action="" method="post" name="login_form">
+        <?php if (isset($msg)) {
     echo $msg;
-}
-    ?>
-		<tr>
-			<td colspan="2" align="left" valign="top">
-				<h3>Login</h3>
-			</td>
-		</tr>
-		<tr>
-			<td align="right" valign="top">Username</td>
-			<td><input name="Username" type="text" class="Input" value="Player"></td>
-		</tr>
-		<tr>
-			<td align="right">Password</td>
-			<td><input name="Password" type="password" class="Input"></td>
-		</tr>
-		<tr>
-			<td>&nbsp;</td>
-			<td><input name="Submit" type="submit" value="Login" class="Button3"></td>
-		</tr>
-		</table>
-	</form>
-	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+}    ?>
+        <h3>Access Control</h3>
+        <div class="form-group">
+          <label for="username"> Username</label>
+          <input name="Username" type="text" class="form-control" value="Player" id="username">
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input name="Password" type="password" class="form-control" id="password">
+        </div>
+        <input name="Submit" type="submit" value="Login" class="btn btn-primary">
+      </form>
+    </div>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </body>
 
 </html>
