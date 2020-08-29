1ccd0eb1bf445ae433e1007f50a2508252f1b447
Ryan Guest <rguest20@googlemail.com>
2020-08-29 18:24:18 +0100

    Cards recreated as objects for OOP.  Started to create interface

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ if (!isset($_SESSION['UserData']['Username'])) {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Negotiation</title>
+  <link href="./css/Chart.css" rel="stylesheet">
   <link href="./css/style2.css" rel="stylesheet">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
@@ -40,8 +41,27 @@ if (!isset($_SESSION['UserData']['Username'])) {
           <table class="table border">
             <thead>
               <td id="interfaceinside">
-                Interface
-                <div id="test"></div>
+                <h3>Interface</h3>
+                <div id="hostages_div" class="border">
+                  <div>
+                    <canvas id="hostages_data_display" aria-label="hostages map" role="img" style="position: relative; height: 8%; width:8%%;">
+                      <p></p>
+                    </canvas>
+                  </div>
+                  <div>
+                    <p><small class="subtitle">Hostages</small></p>
+                  </div>
+                </div>
+                <div id="conversation_points_div" class="border">
+                  <p>Conversation Points:</p>
+                  <p id="conversationPointsP">0</p>
+                </div>
+                <div id="threat_level_div" class="border">
+                  <p>Threat Level</p>
+                  <div class="progress">
+                    <div class="progress-bar bg-success" role="progressbar" style="width: 30%" aria-valuenow="3" aria-valuemin="0" aria-valuemax="10"></div>
+                  </div>
+                </div>
               </td>
             </thead>
           </table>
@@ -87,7 +107,8 @@ if (!isset($_SESSION['UserData']['Username'])) {
                   <table class="table table-bordered innertable">
                     <thead>
                       <tr>
-                        <th class="border" id="cardtablehead"><button class="button btn-sm btn-secondary" id="prevCard" value="previous" onclick="prevCard()"><<</button> <span id="titleOfCard"></span> <button id="nextCard" class="button btn-sm btn-secondary" value="Next" onclick="nextCard()">>></button></th>
+                        <th class="border" id="cardtablehead"><button class="button btn-sm btn-secondary" id="prevCard" value="previous" onclick="prevCard()">
+                            << </button> <span id="titleOfCard"></span> <button id="nextCard" class="button btn-sm btn-secondary" value="Next" onclick="nextCard()">>></button></th>
                       </tr>
                     </thead>
                     <tbody>
@@ -97,6 +118,9 @@ if (!isset($_SESSION['UserData']['Username'])) {
                           <p>On 2 successes: <span id="bigSuccessOutcome"></span></p>
                           <p>On 1 success: <span id="littleSuccessOutcome"></span></p>
                           <p>Failure: <span id="failureOutcome"></span></p>
+                          <p>In Hand: <span id="isInHand"></span></p>
+                          <p> <button id="playCard">Play Card</button> <button id="sacrificeCard">Sacrifice for 1 CP</button></p>
+                          <p> <button id="buyCard">Buy Card</button>
                         </th>
                       </tr>
                     </tbody>
@@ -110,31 +134,193 @@ if (!isset($_SESSION['UserData']['Username'])) {
     </div>
     <p><a href="logout.php">Click here</a> to Logout.</p>
   </div>
-  <script type="text/javascript" src="conversationcards.json"></script>
+  <script type="text/javascript" src="conversationcards.js"></script>
+  <script src="./scripts/helper/Chart.js"></script>
+  <script src="./scripts/helper/moment.min.js"></script>
   <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
   <script>
+    //initialize conversation cards
+    function ConversationCard(id, title, cost, bigSuccess, smallSuccess, failure, endTurn) {
+      this.id = id
+      this.title = title
+      this.cost = cost
+      this.bigSuccess = bigSuccess
+      this.smallSuccess = smallSuccess
+      this.failure = failure
+      this.endTurnIf = endTurn
+    }
+
+    //initilize
+    let hand = [1, 2, 3, 4, 5, 6]
+    let conversationcards = []
+    for (card in data) {
+
+      let bigSuccessForCard = {}
+      data[card]['bigSuccessDice'] != "0" ? bigSuccessForCard.dice = data[card]['bigSuccessDice'] : true
+      data[card]['bigSuccessConversationPoints'] != "0" ? bigSuccessForCard.conversationpoints = data[card]['bigSuccessConversationPoints'] : true
+      data[card]['bigSuccessThreatChange'] != "0" ? bigSuccessForCard.threat = data[card]['bigSuccessThreatChange'] : true
+      data[card]['bigSuccessHostageRelease'] != "0" ? bigSuccessForCard.hostage = data[card]['bigSuccessHostageRelease'] : true
+      data[card]['bigSuccess4to5'] === "true" ? bigSuccessForCard.fourtofive = true : true
+      data[card]['bigSuccessRevealDemand'] === "true" ? bigSuccessForCard.demand = true : true
+      data[card]['bigSuccessAbductorKill'] === "true" ? bigSuccessForCard.abductorkilled = true : bigSuccessForCard.abductorkilled = false
+
+      let smallSuccessForCard = {}
+      data[card]['smallSuccessDice'] != "0" ? smallSuccessForCard.dice = data[card]['smallSuccessDice'] : true
+      data[card]['smallSuccessConversationPoints'] != "0" ? smallSuccessForCard.conversationpoints = data[card]['smallSuccessConversationPoints'] : true
+      data[card]['smallSuccessThreatChange'] != "0" ? smallSuccessForCard.threat = data[card]['smallSuccessThreatChange'] : true
+      data[card]['smallSuccessHostageRelease'] != "0" ? smallSuccessForCard.hostage = data[card]['smallSuccessHostageRelease'] : true
+      data[card]['smallSuccess4to5'] === "true" ? smallSuccessForCard.fourtofive = true : true
+      data[card]['smallSuccessRevealDemand'] === "true" ? smallSuccessForCard.demand = true : true
+      data[card]['smallSuccessAbductorKill'] === "true" ? smallSuccessForCard.abductorkilled = true : smallSuccessForCard.abductorkilled = false
+
+      let failureForCard = {}
+      data[card]['failureDice'] != "0" ? failureForCard.dice = data[card]['failureDice'] : true
+      data[card]['failureConversationPoints'] != "0" ? failureForCard.conversationpoints = data[card]['failureConversationPoints'] : true
+      data[card]['failureThreatChange'] != "0" ? failureForCard.threat = data[card]['failureThreatChange'] : true
+      data[card]['failureHostageKill'] != "0" ? failureForCard.hostage = data[card]['failureHostageKill'] : true
+      data[card]['failureCardRemove'] === "true" ? failureForCard.remove = true : true
+      data[card]['failureAbductorEscape'] === "true" ? failureForCard.abductorescaped = true : failureForCard.abductorescaped = false
+
+      let endTurnIf = ""
+      if (data[card]['bigSuccessConversationEnd'] === "false" && data[card]['smallSuccessConversationEnd'] === "false" && data[card]['failureConversationEnd'] === "false") {
+        endTurnIf = false
+      } else if (data[card]['bigSuccessConversationEnd'] === "true" && data[card]['smallSuccessConversationEnd'] === "true" && data[card]['failureConversationEnd'] === "true") {
+        endTurnIf = "all"
+      } else if (data[card]['bigSuccessConversationEnd'] === "true") {
+        endTurnIf = "big"
+      } else if (data[card]['smallSuccessConversationEnd'] === "true") {
+        endTurnIf = "small"
+      } else {
+        endTurnIf = "failure"
+      }
+
+      let convoCard = new ConversationCard(Number(data[card]['id']), data[card]['title'], data[card]['cost'], bigSuccessForCard, smallSuccessForCard, failureForCard, endTurnIf)
+      conversationcards.push(convoCard)
+    }
+
+    var ctx = $('#hostages_data_display');
+    var myChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        labels: [
+          'Unknown',
+          'Alive',
+          'Dead',
+        ],
+        datasets: [{
+          data: [1, 0, 0],
+          backgroundColor: [
+            'rgba(0, 255, 0, 0.2)',
+            'rgba(0, 0 , 255, 0.2)',
+            'rgba(255, 0 ,0 , 0.2)'
+          ],
+        }]
+      },
+      options: {
+        responsive: true,
+        legend: false
+      }
+    })
+
+    // Creates the view for the conversation cards (bottom right)
+    function setCard() {
+      let currentcard = conversationcards[cardnumber]
+      $('#cardnumber').empty()
+      $('#cardnumber').append(currentcard.id)
+      $('#titleOfCard').empty()
+      $('#titleOfCard').append(currentcard.title)
+      $('#costOfCard').empty()
+      $('#costOfCard').append(currentcard.cost)
+      $('#bigSuccessOutcome').empty()
+      $('#bigSuccessOutcome').append('<ul>')
+      if ('conversationpoints' in currentcard.bigSuccess) {
+        $('#bigSuccessOutcome').append('<li> CP: ' + currentcard.bigSuccess['conversationpoints'] + '</li>')
+      }
+      if ('dice' in currentcard.bigSuccess) {
+        $('#bigSuccessOutcome').append('<li> Dice: ' + currentcard.bigSuccess['dice'] + '</li>')
+      }
+      if ('threat' in currentcard.bigSuccess) {
+        $('#bigSuccessOutcome').append('<li> THR: ' + currentcard.bigSuccess['threat'] + '</li>')
+      }
+      if ('fourtofive' in currentcard.bigSuccess) {
+        $('#bigSuccessOutcome').append("<li> 4's become 5's </li>")
+      }
+      if ('demand' in currentcard.bigSuccess) {
+        $('#bigSuccessOutcome').append('<li> Demand Reveal</li>')
+      }
+      if ('hostage' in currentcard.bigSuccess) {
+        $('#bigSuccessOutcome').append('<li> Hostages Rescued: ' + currentcard.bigSuccess['hostage'] + '</li>')
+      }
+      if (currentcard.bigSuccess['abductorkilled'] === true) {
+        $('#bigSuccessOutcome').append('<li> Abductor Killed </li>')
+      }
+      $('#bigSuccessOutcome').append('</ul>')
+      $('#littleSuccessOutcome').empty()
+      $('#littleSuccessOutcome').append('<ul>')
+      if ('conversationpoints' in currentcard.smallSuccess) {
+        $('#littleSuccessOutcome').append('<li> CP: ' + currentcard.smallSuccess['conversationpoints'] + '</li>')
+      }
+      if ('dice' in currentcard.smallSuccess) {
+        $('#littleSuccessOutcome').append('<li> Dice: ' + currentcard.smallSuccess['dice'] + '</li>')
+      }
+      if ('threat' in currentcard.smallSuccess) {
+        $('#littleSuccessOutcome').append('<li> THR: ' + currentcard.smallSuccess['threat'] + '</li>')
+      }
+      if ('fourtofive' in currentcard.smallSuccess) {
+        $('#littleSuccessOutcome').append("<li> 4's become 5's </li>")
+      }
+      if ('demand' in currentcard.smallSuccess) {
+        $('#littleSuccessOutcome').append('<li> Demand Reveal </li>')
+      }
+      if ('hostage' in currentcard.smallSuccess) {
+        $('#littleSuccessOutcome').append('<li> Hostages Rescued: ' + currentcard.smallSuccess['hostage'] + '</li>')
+      }
+      if (currentcard.smallSuccess['abductorkilled'] === true) {
+        $('#littleSuccessOutcome').append('<li> Abductor Killed </li>')
+      }
+      $('#littleSuccessOutcome').append('</ul>')
+      $('#failureOutcome').empty()
+      $('#failureOutcome').append('<ul>')
+      if ('conversationpoints' in currentcard.failure) {
+        $('#failureOutcome').append('<li> CP: ' + currentcard.failure['conversationpoints'] + '</li>')
+      }
+      if ('dice' in currentcard.failure) {
+        $('#failureOutcome').append('<li> Dice: ' + currentcard.failure['dice'] + '</li>')
+      }
+      if ('threat' in currentcard.failure) {
+        $('#failureOutcome').append('<li> THR: ' + currentcard.failure['threat'] + '</li>')
+      }
+      if ('fourtofive' in currentcard.failure) {
+        $('#failureOutcome').append("<li> 4's become 5's </li>")
+      }
+      if ('remove' in currentcard.failure) {
+        $('#failureOutcome').append('<li> Card Lost Permanently </li>')
+      }
+      if ('hostage' in currentcard.failure) {
+        $('#failureOutcome').append('<li> Hostages Killed: ' + currentcard.failure['hostage'] + '</li>')
+      }
+      if (currentcard.failure['abductorescaped'] === true) {
+        $('#failureOutcome').append('<li> GAME OVER </li>')
+        $('#failureOutcome').append('</ul>')
+      }
+      $('#isInHand').empty()
+      $('#isInHand').append('No')
+      for (id in hand) {
+        if (Number(hand[id]) === currentcard.id) {
+          $('#isInHand').empty()
+          $('#isInHand').append('Yes')
+        }
+      }
+    }
+
+    // allow player to scroll through conversation cards
     let cardnumber = 0
     setCard()
 
-    function setCard(){
-    $('#cardnumber').empty()
-    $('#cardnumber').append(Number(data[cardnumber]['id']))
-    $('#titleOfCard').empty()
-    $('#titleOfCard').append(data[cardnumber]['title'])
-    $('#costOfCard').empty()
-    $('#costOfCard').append(data[cardnumber]['cost'])
-    $('#bigSuccessOutcome').empty()
-    $('#bigSuccessOutcome').append('TBI')
-    $('#littleSuccessOutcome').empty()
-    $('#littleSuccessOutcome').append('TBI')
-    $('#failureOutcome').empty()
-    $('#failureOutcome').append('TBI')
-  }
-
-    function nextCard(){
-      if (cardnumber === 19){
+    function nextCard() {
+      if (cardnumber === 19) {
         return
       } else {
         cardnumber++
@@ -142,7 +328,7 @@ if (!isset($_SESSION['UserData']['Username'])) {
       }
     }
 
-    function prevCard(){
+    function prevCard() {
       if (cardnumber === 0) {
         return
       } else {
@@ -151,6 +337,8 @@ if (!isset($_SESSION['UserData']['Username'])) {
       }
     }
 
+
+    //functions to roll the dice for the game
     function randomised6() {
       let randomnumber = Math.ceil(Math.random() * 6)
       return randomnumber
