a18b70d5aaf6ebdc9478a6114eb959afa2ec88dc
Ryan Guest <rguest20@googlemail.com>
2021-02-06 16:31:46 +0000

    Code checked with Javascript linter and W3C Validator.  Changes made to make it complient.

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,15 @@
 $('#fireworks').hide()
-console.log("I am running")
-//hide splash screen - comment out to show
+// hide splash screen - comment out to show
 // $('#splash').hide()
 $('#textbox').hide()
 delayanimation(events.preparegame, 1000)
-//show splash screen
-window.onload = function() {
+// show splash screen
+window.onload = function () {
   $('#titleofgame').addClass('textappear')
   $('#splash').addClass('titlecarddisappear')
 }
 
-//initial preparation for game
+// initial preparation for game
 events.updatetime()
 diceingame.set()
 // updatethreat()
@@ -28,80 +27,80 @@ let currentterror
 let terroroutcome
 let terroroutcomepass
 let terroroutcomefail
-let threatoutcome = "not required"
+let threatoutcome = 'not required'
 
-function unlockterrorbutton(){
+function unlockterrorbutton () {
   $('#acceptterror').prop('disabled', false)
 }
-//set buttons to be disabled
+// set buttons to be disabled
 $('#playcard').prop('disabled', true)
 $('#fourtofivebutton').prop('disabled', true)
 $('#reroll').prop('disabled', true)
 
-//random number generator
-function getrandomint(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+// random number generator
+function getrandomint (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min
 }
 
-function showbutton() {
+function showbutton () {
   $('#buttoncontinue').show()
 }
 
-function showbutton2() {
+function showbutton2 () {
   $('#buttonchoice1').show()
 }
 
-function showbutton3() {
+function showbutton3 () {
   $('#buttonchoice2').show()
 }
 
-//typing function to write messages in a more interesting way
-function typeout(message, position) {
+// typing function to write messages in a more interesting way
+function typeout (message, position) {
   let messagetext = message
   let i = 0
   let result = messagetext[i]
-  let typing = setInterval(function() {
-      if (i == messagetext.length-1) {
-        clearInterval(typing)
-        return;
-      }
-      i++
-      result += messagetext[i]
-      position.html(result)
-    },
-    30)
+  let typing = setInterval(function () {
+    if (i === messagetext.length - 1) {
+      clearInterval(typing)
+      return
+    }
+    i++
+    result += messagetext[i]
+    position.html(result)
+  },
+  30)
 }
 
-//arrows to scroll through cards
-function nextCard() {
+// arrows to scroll through cards
+function nextCard () {
   if (events.currentcard === (conversationcards.length-1)) {
     return
   } else {
-    events.currentcard ++
+    events.currentcard++
     conversationcards[events.currentcard].setCard()
   }
 }
 
-function prevCard() {
+function prevCard () {
   if (events.currentcard === 0) {
     return
   } else {
-    events.currentcard --
+    events.currentcard--
     conversationcards[events.currentcard].setCard()
   }
 }
 
-//Playing cards - several functions
-function buttondisable1(){
+// Playing cards - several functions
+function buttondisable1 () {
   $('#playcard').prop('disabled', false)
 }
 
-function buttondisable2(){
+function buttondisable2 (){
   $('#fourtofivebutton').prop('disabled', false)
 }
 
 function countboxes(){
-  let boxchecknumber = $(":checkbox:checked").length
+  let boxchecknumber = $(':checkbox:checked').length
   if (boxchecknumber === 2){
     $('#discardbutton').prop('disabled', false)
   } else {
@@ -110,10 +109,10 @@ function countboxes(){
 }
 
 function delayanimation(funct, val) {
-  let waittime = new Promise(function(resolve) {
+  let waittime = new Promise(function (resolve) {
     resolve(setTimeout(funct, val))
   })
-  waittime.then(function() {
+  waittime.then(function () {
     clearTimeout()
   })
 }
--- scripts/viewhandler.js
+++ scripts/viewhandler.js
@@ -1,9 +1,9 @@
-function EventHandler(){
-  this.randomnumber = function(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+function EventHandler () {
+  this.randomnumber = function (min, max) {
+    return Math.floor(Math.random() * (max - min)) + min
   }
   this.terrorsuccess = false
-  this.currentterror ={}
+  this.currentterror = {}
   this.abductoralive = true
   this.nomorehostages = false
   this.turnsleft = 10
@@ -13,17 +13,17 @@ function EventHandler(){
   this.demand2flipped = false
   // this.hand = [1,2,3,4,5,6]
   this.threatperturn = 0
-  this.concedebutton = function(number){
-    if (conversationpoints >= demandcardsingame[(number-1)].cost){
-      conversationpoints -= demandcardsingame[(number-1)].cost
-      demandcardsingame[(number-1)].concede()
+  this.concedebutton = function (number) {
+    if (conversationpoints >= demandcardsingame[(number - 1)].cost) {
+      conversationpoints -= demandcardsingame[(number - 1)].cost
+      demandcardsingame[(number - 1)].concede()
       $('#conversationPointsP').html(conversationpoints)
       $('#concedebutton' + number).prop('disabled', true)
-      $('#concedebutton' + number).html("Conceded")
+      $('#concedebutton' + number).html('Conceded')
     }
   }
-  this.preparedemands = function(){
-    if (hostagetakerarray[1].name === "Ann Greashopper") {
+  this.preparedemands = function () {
+    if (hostagetakerarray[1].name === 'Ann Greashopper') {
       demandcardsingame.push(demandcards[0])
       demandcardsingame.push(demandcards[this.randomnumber(4, 7)])
       this.setdemandcards(demandcardsingame[0].id, demandcardsingame[1].id)
@@ -33,96 +33,96 @@ function EventHandler(){
       this.setdemandcards()
     }
   }
-  this.setdemandcards = function (){
+  this.setdemandcards = function () {
     demandcardsingame[0].setreward()
     demandcardsingame[1].setreward()
     $('#demand1title').html(demandcardsingame[0].title)
     $('#demand1cost').html(' CP to concede: ' + demandcardsingame[0].cost)
-    for (i=0; i<demandcardsingame[0].rewards.length; i++){
-      $('#demand1reward').append(demandcardsingame[0].rewards[i] + " ")
+    for (i = 0; i < demandcardsingame[0].rewards.length; i++) {
+      $('#demand1reward').append(demandcardsingame[0].rewards[i] + ' ')
     }
     $('#demand1penalty').html('HOWEVER ' + demandcardsingame[0].setpenalty())
     $('#demand2title').html(demandcardsingame[1].title)
     $('#demand2cost').html(' CP to concede: ' + demandcardsingame[1].cost)
-    for (i=0; i<demandcardsingame[1].rewards.length; i++){
-      $('#demand2reward').append(demandcardsingame[1].rewards[i] + " ")
+    for (i = 0; i < demandcardsingame[1].rewards.length; i++) {
+      $('#demand2reward').append(demandcardsingame[1].rewards[i] + ' ')
     }
     $('#demand2penalty').html('HOWEVER ' + demandcardsingame[1].setpenalty())
   }
-  this.gameover = function(){
-    if (this.abductoralive === false && this.nomorehostages === true){
-      let numbersaved = hostagechart['data']["datasets"][0]["data"][0]
-      $('#winlosemodal').modal({backdrop: false, keyboard: false})
-      if ((parseFloat(numbersaved)/parseFloat(hostagetakeringame.numberofhostages)) < 0.5){
-        $('#winorlose').html("Pyrrhic Victory")
+  this.gameover = function () {
+    if (this.abductoralive === false && this.nomorehostages === true) {
+      let numbersaved = hostagechart['data']['datasets'][0]['data'][0]
+      $('#winlosemodal').modal({ backdrop: false, keyboard: false })
+      if ((parseFloat(numbersaved) / parseFloat(hostagetakeringame.numberofhostages)) < 0.5) {
+        $('#winorlose').html('Pyrrhic Victory')
         $('#winorloseflufftext').html("Well, captain. You captured the hostage taker, but at what cost?  I don't think top brass will be happy with this.")
-      } else if ((parseFloat(numbersaved)/parseFloat(hostagetakeringame.numberofhostages)) < 0.95){
-        $('#winorlose').html("Victory")
-        $('#winorloseflufftext').html("Well done, Cap! You captured the hostage taker, and even managed to save over half of the hostages! A fantastic job!.")
+      } else if ((parseFloat(numbersaved) / parseFloat(hostagetakeringame.numberofhostages)) < 0.95) {
+        $('#winorlose').html('Victory')
+        $('#winorloseflufftext').html('Well done, Cap! You captured the hostage taker, and even managed to save over half of the hostages! A fantastic job!.')
       } else {
-        $('#winorlose').html("Amazing Victory!")
-        $('#winorloseflufftext').html("Just wonderful, Cap! You captured the hostage taker, saved the day and all of the hostages to boot! I think you have a medal in your future!.")
+        $('#winorlose').html('Amazing Victory!')
+        $('#winorloseflufftext').html('Just wonderful, Cap! You captured the hostage taker, saved the day and all of the hostages to boot! I think you have a medal in your future!.')
       }
       $('#howmanyhostagessaved').html(numbersaved)
       $('#howmanyhostageskilled').html(hostagetakeringame.number - numbersaved)
       $('#abductoralive').html('Yes')
       $('#fireworks').show()
     } else {
-      if (this.turnsleft >= 0){
+      if (this.turnsleft >= 0) {
         return
       } else {
-        let numbersaved = hostagechart['data']["datasets"][0]["data"][0]
-        let numberkilled = hostagechart['data']["datasets"][0]["data"][2]
-        $('#winlosemodal').modal({backdrop: false, keyboard: false})
-        $('#winorlose').html("Game Over")
-        $('#winorloseflufftext').html("Oh my, captain.  That didn't go too well did it?  Remember, that the most important thing is to bring in the abductor. This will be a tough one to explain...")
+        let numbersaved = hostagechart['data']['datasets'][0]['data'][0]
+        let numberkilled = hostagechart['data']['datasets'][0]['data'][2]
+        $('#winlosemodal').modal({ backdrop: false, keyboard: false })
+        $('#winorlose').html('Game Over')
+        $('#winorloseflufftext').html('Oh my, captain.  That didn\'t go too well did it?  Remember, that the most important thing is to bring in the abductor. This will be a tough one to explain...')
         $('#howmanyhostagessaved').html(numbersaved)
         $('#howmanyhostageskilled').html(numberkilled)
         $('#abductoralive').html('No')
       }
     }
   }
-  this.interfacehelp = ""
-  this.interfaceabductor = ""
-  this.interfacenegotiator = ""
-  this.updatetime = function(){
+  this.interfacehelp = ''
+  this.interfaceabductor = ''
+  this.interfacenegotiator = ''
+  this.updatetime = function () {
     if (this.turnsleft >= 0) {
-      $('#turnsleft').html('<strong>Turns remaining: ' + this.turnsleft + "</strong>")
+      $('#turnsleft').html('<strong>Turns remaining: ' + this.turnsleft + '</strong>')
     } else {
       this.gameover()
     }
   }
-  this.displayterror = async function(){
+  this.displayterror = async function () {
     this.currentterror = terrorcarddeck.pop()
     $('#terrortitle').html(this.currentterror.title)
-    if (this.currentterror.diceroll === true){
-      let successtext = ""
-      let failtext = ""
-      switch(this.currentterror.threatsuccess) {
+    if (this.currentterror.diceroll === true) {
+      let successtext = ''
+      let failtext = ''
+      switch (this.currentterror.threatsuccess) {
         case 'extrahostage':
-          successtext = "An extra hostage is taken"
+          successtext = 'An extra hostage is taken'
           terroroutcomepass = '1hostage'
-          break;
+          break
         case 'noeffect':
           successtext = 'No effect this time'
           terroroutcomepass = 'noeffect'
-          break;
+          break
         case 'hostageescape':
           successtext = 'The hostage escapes'
           terroroutcomepass = '1escape'
-          break;
+          break
         default:
           false
       }
-      switch(this.currentterror.threatfail) {
+      switch (this.currentterror.threatfail) {
         case 'extrahostage':
-          failtext = "Two extra hostages are taken"
+          failtext = 'Two extra hostages are taken'
           terroroutcomefail = '2hostage'
-          break;
+          break
         case 'hostagekilled':
           failtext = 'A hostage is killed'
           terroroutcomefail = '1dead'
-          break;
+          break
         default:
           false
       }
@@ -130,82 +130,82 @@ function EventHandler(){
       let terrordice = diceingame.roll()
       $('#acceptterror').prop('disabled', true)
       await delayanimation(unlockterrorbutton, 2000)
-      for (i=0; i<diceingame.number;i++){
-        if (terrordice[i] > 4){
+      for (i = 0; i < diceingame.number; i++) {
+        if (terrordice[i] > 4) {
           this.terrorsuccess = true
-        } else{
+        } else {
           this.terrorsuccess = false
         }
       }
     } else {
-      let effecttext = ""
-      switch(this.currentterror.effect) {
+      let effecttext = ''
+      switch (this.currentterror.effect) {
         case 'hostagekilled: 1':
-          effecttext = "A hostage is killed"
+          effecttext = 'A hostage is killed'
           terroroutcomepass = '1dead'
-          break;
+          break
         case 'threat: 1':
           effecttext = 'Threat increases by 1'
           terroroutcomepass = '1threat'
-          break;
+          break
         case 'threat: 2':
           effecttext = 'Threat increases by 2'
           terroroutcomepass = '2threat'
-          break;
+          break
         case 'timeremaining: -1':
           effecttext = 'Time remaining decreases by 1'
           terroroutcomepass = '-1time'
-          break;
+          break
         case 'timeremaining: half':
           effecttext = 'Time remaining halves(rounding up)'
           terroroutcomepass = 'halftime'
-          break;
+          break
         case 'hostageescape: 1':
           effecttext = 'A hostage is freed'
           terroroutcomepass = '1free'
-          break;
+          break
         case 'threat: reset':
           effecttext = 'Threat is reset to 4'
           terroroutcomepass = 'resetthreat'
-          break;
+          break
         default:
           false
       }
       $('#terroreffect').html(effecttext)
     }
-    $('#terrormodal').modal({backdrop: false, keyboard: false})
+    $('#terrormodal').modal({ backdrop: false, keyboard: false })
   }
-  this.preparegame = function(){
-        hostagetakeringame = hostagetakerarray[1]
-        hostagetakeringame.sethostages(0, hostagetakeringame.number, 0)
-        $('#textbox').hide()
-        $('#hostagetakername').html(hostagetakeringame.name)
-        $('#whatweknow').html(hostagetakeringame.description)
-        events.preparedemands()
-    }
-  this.phaseone = async function(){
+  this.preparegame = function () {
+    hostagetakeringame = hostagetakerarray[1]
+    hostagetakeringame.sethostages(0, hostagetakeringame.number, 0)
+    $('#textbox').hide()
+    $('#hostagetakername').html(hostagetakeringame.name)
+    $('#whatweknow').html(hostagetakeringame.description)
+    events.preparedemands()
+  }
+  this.phaseone = async function () {
     $('#phase3bar').removeClass('activated')
     $('#phase1bar').addClass('activated')
     $('#playphasebuttons').show()
     $('#buyphasebuttons').hide()
     $('#endphase1').show()
     $('#endphase2').hide()
-    for (i = 0; i< player.discard.length; i++){
+    for (i = 0; i < player.discard.length; i++) {
       player.availabletobuy.push(player.discard[i])
     }
-    while(player.discard.length > 0) {
-      player.discards.pop();
+    while (player.discard.length > 0) {
+      player.discards.pop()
     }
-    if (document.getElementById('isInHand').innerHTML === "Discarded"){
-      $('#isInHand').html("Available to buy")
+    if (document.getElementById('isInHand').innerHTML === 'Discarded') {
+      $('#isInHand').html('Available to buy')
     }
     this.conversationpoints = 0
     $('#conversationPointsP').html(this.conversationpoints)
-    if (this.threatperturn != 0){
+    if (this.threatperturn !== 0) {
       threat.change(this.threatperturn)
     }
   }
-  this.phasetwo = function(){
+  this.phasetwo = function () {
     $('#phase1bar').removeClass('activated')
     $('#phase2bar').addClass('activated')
     $('#playphasebuttons').hide()
@@ -213,22 +213,22 @@ function EventHandler(){
     $('#endphase1').hide()
     $('#endphase2').show()
   }
-  this.phasethree =  async function() {
-      $('#phase2bar').removeClass('activated')
-      $('#phase3bar').addClass('activated')
-      $('#buyphasebuttons').hide()
-      $('#endphase2').hide()
-      $('#endphase3').show()
-      this.turnsleft -= 1
-      this.updatetime()
-      this.displayterror()
+  this.phasethree = async function () {
+    $('#phase2bar').removeClass('activated')
+    $('#phase3bar').addClass('activated')
+    $('#buyphasebuttons').hide()
+    $('#endphase2').hide()
+    $('#endphase3').show()
+    this.turnsleft -= 1
+    this.updatetime()
+    this.displayterror()
   }
-  this.pressconcede1 = function(){
-    $('#demand1').removeClass("flip-card-toggled")
+  this.pressconcede1 = function () {
+    $('#demand1').removeClass('flip-card-toggled')
     demandcardsingame[0].concede()
   }
-  this.pressconcede2 = function(){
-    $('#demand2').removeClass("flip-card-toggled")
+  this.pressconcede2 = function () {
+    $('#demand2').removeClass('flip-card-toggled')
     demandcardsingame[1].concede()
   }
 }
