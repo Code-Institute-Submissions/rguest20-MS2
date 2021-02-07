6fc5385e040ed260e1dc9ca06e9a446da7186671
Ryan Guest <rguest20@googlemail.com>
2021-02-07 11:19:05 +0000

    fixed terrorcard shuffle

--- scripts/terrorcards.js
+++ scripts/terrorcards.js
@@ -204,7 +204,7 @@ shuffle(terrorcarddeck)
 
 function shuffle (array) {
   for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1))
+    let j = Math.floor(Math.random() * (i + 1))
       [array[i], array[j]] = [array[j], array[i]]
   }
 }
